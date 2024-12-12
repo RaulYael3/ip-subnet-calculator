01c83d5e4ec9004c0412063f07d284c23b9a46ed
Yael <rayacova37@gmail.com>
2024-12-12 17:37:39 -0600

    loop refactoring

--- src/services/fillTables.ts
+++ src/services/fillTables.ts
@@ -9,39 +9,27 @@ export default function fillTables(ip: IPv4, totalSubnets: number, subnet: numbe
     }
     
 
-    let i = 0
     const rows = []
-    while (i < 5 && i < subnet) {
-        const subNetwork = [...manipulableIP]
-        const networkStart = [...subNetwork]
-        const networkEnd = [...subNetwork]
-        const broadcast = [...subNetwork]
-       
-        networkStart[3] += 1;
-
-        if(indexFinded < 3) {
+    for (let i = 0; i < Math.min(5, subnet); i++) {
+        const subNetwork = [...manipulableIP];
+        const networkStart = [...subNetwork];
+        const networkEnd = [...subNetwork];
+        const broadcast = [...subNetwork];
+    
+        networkStart[3]++;
+        if (indexFinded < 3) {
             networkEnd[3] = 254;
             broadcast[3] = 255;
-        } else{
+        } else {
             networkEnd[3] += totalSubnets - 2;
             broadcast[3] += totalSubnets - 1;
         }
-
+    
         rows.push({
-            ips: [
-                subNetwork.join('.'),
-                networkStart.join('.'),
-                networkEnd.join('.'),
-                broadcast.join('.')
-            ]
-        })
-
-        if (indexFinded < 3){
-           manipulableIP[indexFinded] += totalSubnets
-        } else {    
-           manipulableIP[3] += totalSubnets 
-        }
-        i++
+            ips: [subNetwork.join('.'), networkStart.join('.'), networkEnd.join('.'), broadcast.join('.')]
+        });
+    
+        manipulableIP[indexFinded < 3 ? indexFinded : 3] += totalSubnets;
     }
 
     return rows
