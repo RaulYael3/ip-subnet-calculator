4e28c9b28bcc06fba38215719673fb156176fe79
Raul Yael <rayacova37@gmail.com>
2024-12-30 15:35:08 -0600

    refactored code, divide the calculates in to functions

--- src/services/fillTables.ts
+++ src/services/fillTables.ts
@@ -16,60 +16,19 @@ function adjustTotalSubnets(totalSubnets: number): number {
   return totalSubnets
 }
 
-function createRow(
+function createRowSub(
           ipArray: number[], 
           totalSubnets: number, 
-          type: 'sub' | 'host', 
           indexFinded: number, 
-          currOctate: number,
-          subnet: number
         ): { ips: string[] }[] 
             {
 
   const rows: { ips: string[] }[] = []
-  const subNetwork = [...ipArray]
-  let changeIndex =3
+
   
   
   for (let i = 0; i < Math.min(20, totalSubnets); i++) {
-    
-    if (type === 'host') {
-      const networkStart = [...subNetwork]
-      const broadcast = [...subNetwork]
-      if(subnet > 255){
-        subnet = totalSubnets / 255
-        changeIndex = 2
-        if(subnet >255) { changeIndex = 1}
-      }
-      
-      networkStart[3]++
-       
-      changeIndex !== 3 
-      ? (broadcast[changeIndex] += totalSubnets - 1,
-        broadcast[3] = 255)
-        : (broadcast[3] += totalSubnets - 1)
-        
-        
-      const networkEnd = [...broadcast]
-      networkEnd[3]--
-      
-      rows.push({ ips: [subNetwork.join('.'), networkStart.join('.'), networkEnd.join('.'), broadcast.join('.')] })
-      
-      subNetwork[changeIndex] += totalSubnets
-      
-      if (subNetwork[changeIndex] >= 255) {
-        subNetwork[changeIndex] = 0 
-        subNetwork[currOctate]  === 0 ? (subNetwork[currOctate] = 0) : subNetwork[currOctate]++
-        subNetwork[changeIndex - 1]++
-        if (subNetwork[currOctate] === 255) {
-          currOctate--
-          console.log('kkk')
-        }
-      }
-
-    } 
-    else
-    {  
+     
       const subNetwork = [...ipArray]
       const networkStart = [...subNetwork]
       const broadcast = [...subNetwork]
@@ -85,10 +44,58 @@ function createRow(
 
       rows.push({ ips: [subNetwork.join('.'), networkStart.join('.'), networkEnd.join('.'), broadcast.join('.')] })
       ipArray[indexFinded < 3 ? indexFinded : 3] += totalSubnets
-    }
+      if (ipArray[indexFinded < 3 ? indexFinded : 3] >= 255) break
+    
   }
-
   return rows
+}
+
+function createRowHost(
+          ipArray: number[], 
+          totalSubnets: number,
+          currOctate: number,
+          subnet: number
+        ): { ips: string[] }[] 
+        {
+      const rows: { ips: string[] }[] = []
+      const subNetwork = [...ipArray]
+      let changeIndex =3
+      for (let i = 0; i < Math.min(20, totalSubnets); i++) {
+
+        const networkStart = [...subNetwork]
+        const broadcast = [...subNetwork]
+        if(subnet > 255){
+          subnet = totalSubnets / 255
+          changeIndex = 2
+          if(subnet >255) { changeIndex = 1}
+        }
+
+        networkStart[3]++
+
+        changeIndex !== 3 
+        ? (broadcast[changeIndex] += totalSubnets - 1,
+          broadcast[3] = 255)
+          : (broadcast[3] += totalSubnets - 1)
+
+
+        const networkEnd = [...broadcast]
+        networkEnd[3]--
+        
+        rows.push({ ips: [subNetwork.join('.'), networkStart.join('.'), networkEnd.join('.'), broadcast.join('.')] })
+        
+        subNetwork[changeIndex] += totalSubnets
+        
+        if (subNetwork[changeIndex] >= 255) {
+          subNetwork[changeIndex] = 0 
+          subNetwork[currOctate]  === 0 ? (subNetwork[currOctate] = 0) : subNetwork[currOctate]++
+          subNetwork[changeIndex - 1]++
+          if (subNetwork[currOctate] === 255) {
+            currOctate--
+
+          }
+    } 
+  }
+    return rows
 }
 
 export default function fillTables(ip: IPv4, totalSubnets: number, type: 'sub' | 'host', subnets:number) {
@@ -96,5 +103,7 @@ export default function fillTables(ip: IPv4, totalSubnets: number, type: 'sub' |
   const indexFinded = findZeroIndex(manipulableIP)
   const adjustedSubnets = adjustTotalSubnets(totalSubnets)
 
-  return createRow(manipulableIP, adjustedSubnets, type, indexFinded, 2, subnets)
+  return type === 'sub' 
+    ? createRowSub(manipulableIP,adjustedSubnets,indexFinded)
+    : createRowHost(manipulableIP, totalSubnets,2,subnets)
 }
